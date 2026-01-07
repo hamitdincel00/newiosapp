a53506005696dde5564ef5bd2b62e83ec6abf2d1
hamitdincel00 <app.tebilisim@gmail.com>
2026-01-08 02:33:06 +0300

    Initial Commit

---
+++ YozgatHakimiyet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YozgatHakimiyet
+//
+//  Created by Hamit DİNCEL  on 8.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YozgatHakimiyet/YozgatHakimiyetApp.swift
@@ -0,0 +1,17 @@
+//
+//  YozgatHakimiyetApp.swift
+//  YozgatHakimiyet
+//
+//  Created by Hamit DİNCEL  on 8.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct YozgatHakimiyetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
